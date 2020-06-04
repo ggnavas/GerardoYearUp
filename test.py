4ee5f2f1926d38f5bbfc36a0faf284de7a80b69b
Gerardo Guzman-Navas <ggnavas@gmail.com>
2020-06-04 09:28:27 -0700

    indentation to print

---
+++ test.py
@@ -0,0 +1,28 @@
+
+#make an array for the sorting of the selection
+
+array = [13, 4, 9, 5, 3, 16, 12]
+
+def selectionSort(array):
+
+    n = len(array)
+
+    for i in range(n): #whatever the length of the array is how many times you are going to run the for loop
+
+        #Initially assume the first element of the unsorted part as the minimum
+        
+        minimum = 1
+
+        for j in range(i+1), n):
+
+            if (array[j] < array[minimum]): #comparison operator
+
+            minimum = j
+
+        temp = array[i]
+        array[i] = array[minimum]
+        array[minimum] = temp
+
+    return array
+
+print(selectionSort(array))
