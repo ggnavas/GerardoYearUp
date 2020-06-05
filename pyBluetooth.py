fa2769abc586dadd51b4ea5b0559800048f14d04
Gerardo Guzman-Navas <ggnavas@gmail.com>
2020-06-05 09:07:37 -0700

    Adding import time

--- pyBluetooth.py
+++ pyBluetooth.py
@@ -1,10 +1,10 @@
 #This script that advertises a bluetooth low energy beacon for 15 seconds
 
+import time
 #importing from libr
 from bluetooth.ble import BeaconService #<-- importing a third party module
 
 #Create an instance of the object from the 3rd party class
-
 service = BeaconService()
 
 #start advertizing the signal
@@ -12,6 +12,6 @@ service.start_advertising ("11111111-2222-3333-4444-555555555555", 1, 1,1,200)
 #setting a 15 timer
 time.sleep(15)
 #stop it after 15 seconds
-service.stop_advertizing
+service.stop_advertizing()
 #display Done
 print("Done.")
